973cab629c238189f4bea989ebf1c251047c43be
Gopish Monga <gopishmonga@gmail.com>
2018-11-03 22:37:48 +0530

    pushing src files

---
+++ src/CDRConstants.java
@@ -0,0 +1,9 @@
+public class CDRConstants {
+  public static int fromPhoneNumber = 0;
+  public static int toPhoneNumber = 1;
+  public static int callStartDate = 2;
+  public static int callStartTime = 3;
+  public static int callEndDate = 4;
+  public static int callEndTime = 5;
+  public static int STDFlag = 6;
+}
